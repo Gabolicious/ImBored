34d1d9d0b54d1469bded1c251a1fb9a4f3d27d17
Gabe Corridore <ohgabriel5@gmail.com>
2021-02-01 11:16:51 -0500

    created activity page w/o reviews

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -4,6 +4,65 @@ rhit.FB_COLLECTION_ACTIVITIES = "activities";
 rhit.FB_KEY_TYPE = "type"
 rhit.FB_KEY_PRICE = "price"
 rhit.FB_KEY_PARTICPANTS = "participants"
+rhit.FB_KEY_ACTIVITY = "activity"
+rhit.FB_KEY_AVAILABILITY = "availability"
+
+rhit.FbActivityManager = class {
+	constructor(id) {
+		this._documentSnapshot = {};
+		this._unsubscribe = null;
+		this._ref = firebase.firestore().collection(rhit.FB_COLLECTION_ACTIVITIES).doc(id)
+	}
+	beginListening(changeListener) {
+		this._unsubscribe = this._ref.onSnapshot((doc) => {
+			if (doc.exists) {
+				this._documentSnapshot = doc;
+				console.log(doc.data());
+			} else {
+				console.log("No such document!");
+			}
+			changeListener()
+		})
+	}
+	stopListening() {
+		this._unsubscribe();
+	}
+
+	get activity() {
+		return this._documentSnapshot.get(rhit.FB_KEY_ACTIVITY)
+	}
+
+	get type() {
+		return this._documentSnapshot.get(rhit.FB_KEY_TYPE)
+	}
+
+	get price() {
+		return this._documentSnapshot.get(rhit.FB_KEY_PRICE)
+	}
+
+	get participants() {
+		return this._documentSnapshot.get(rhit.FB_KEY_PARTICPANTS)
+	}
+
+	get availability() {
+		return this._documentSnapshot.get(rhit.FB_KEY_AVAILABILITY)
+	}
+}
+
+rhit.ActivityPageController = class {
+	constructor() {
+		rhit.fbActivityManager.beginListening(this.updateView.bind(this))
+	}
+
+	updateView() {
+		document.getElementById("activity-title").innerHTML = rhit.fbActivityManager.activity;
+		document.getElementById("type").innerHTML = `Type: ${rhit.fbActivityManager.type}`
+		document.getElementById("participants").innerHTML = `Participants: ${rhit.fbActivityManager.participants}`
+		document.getElementById("price-slider").value = rhit.fbActivityManager.price;
+		document.getElementById("access-slider").value = rhit.fbActivityManager.availability;
+
+	}
+}
 
 rhit.FbActivitiesManager = class {
 	constructor() {
@@ -11,27 +70,31 @@ rhit.FbActivitiesManager = class {
 	}
 
 	getRandomActivity(type, price, participants) {
-		console.log('type :>> ', type);
-		console.log('price :>> ', price);
-		console.log('participants :>> ', participants);
-		let query = this._ref.where(rhit.FB_KEY_PRICE, "<=", price)
-		//query = query.where(rhit.FB_KEY_PARTICPANTS, ">=", participants)
-		if (type != "any") {
-			query = query.where(rhit.FB_KEY_TYPE, "==", type)
-		}
-		
-		query.get().then((querySnapshots) => {
-			const possibleSnapshots = []
-			querySnapshots.forEach(doc => {
-				if (doc.data()[rhit.FB_KEY_PARTICPANTS] >= participants) possibleSnapshots.push(doc.id)
-			});
-			if (possibleSnapshots.length < 1) {
-				return
+		return new Promise((resolve, reject) => {
+			console.log('type :>> ', type);
+			console.log('price :>> ', price);
+			console.log('participants :>> ', participants);
+			let query = this._ref.where(rhit.FB_KEY_PRICE, "<=", price)
+			//query = query.where(rhit.FB_KEY_PARTICPANTS, ">=", participants)
+			if (type != "any") {
+				query = query.where(rhit.FB_KEY_TYPE, "==", type)
 			}
-			return possibleSnapshots[Math.floor(Math.random() * possibleSnapshots.length)]
-		}).catch(function(error) {
-			console.log("Error getting documents: ", error);
-		});
+
+			query.get().then((querySnapshots) => {
+				const possibleSnapshots = []
+				querySnapshots.forEach(doc => {
+					if (doc.data()[rhit.FB_KEY_PARTICPANTS] >= participants) possibleSnapshots.push(doc.id)
+				});
+				console.log(possibleSnapshots);
+				if (possibleSnapshots.length < 1) {
+					reject("No activity could be found! Try a broader search")
+				}
+				resolve(possibleSnapshots[Math.floor(Math.random() * possibleSnapshots.length)])
+			}).catch(function (error) {
+				console.log("Error with firestore ", error);
+				reject("Error getting an activity")
+			});
+		})
 	}
 }
 
@@ -42,7 +105,7 @@ rhit.HomePageController = class {
 			const type = document.getElementById("type-select").value.toLowerCase();
 			const price = parseFloat(document.getElementById("price-slider").value);
 			const participants = parseInt(document.getElementById("participant-input").value);
-			
+
 			if (participants < 1) {
 				alert("Please select a valid number of participants")
 				return;
@@ -56,23 +119,26 @@ rhit.HomePageController = class {
 				return;
 			}
 
-			const randomActivity = rhit.fbActivitiesManager.getRandomActivity(type, price, participants)
-			if (!randomActivity) {
-				alert("No activity could be found! Try a broader search")
-				return;
-			}
-			window.location.href = `/activity.html?id=${randomActivity}`
+			rhit.fbActivitiesManager.getRandomActivity(type, price, participants).then((randomActivity) => {
+				window.location.href = `/activity.html?id=${randomActivity}`
+			}).catch((error) => {
+				alert(error)
+			})
 		}
 	}
 }
 
 rhit.main = function () {
 	console.log("Ready");
+	const urlParams = new URLSearchParams(window.location.search)
 	if (document.getElementById("home-page")) {
 		rhit.fbActivitiesManager = new rhit.FbActivitiesManager()
 		new rhit.HomePageController();
+	} else if (document.getElementById("activity-page")) {
+		rhit.fbActivityManager = new rhit.FbActivityManager(urlParams.get("id"))
+		new rhit.ActivityPageController()
 	}
-	
+
 };
 
 rhit.main();
